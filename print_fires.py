7957ddacc40a0de690e0718df4179eb4d377989d
theurdragon <theurdragon@theurdragons-MacBook-Pro.local>
2026-08-27 14:09:10 -0600

    fix use of get_cols

--- print_fires.py
+++ print_fires.py
@@ -1,6 +1,9 @@
+from my_utils import get_column
+
 country='United States of America'
-county_column = 1
-fires_column = 4
+country_column = 0
+fires_column = 3
 file_name = 'Agrofood_co2_emission.csv'
-fires = get_column()
+fires = get_column(file_name, country_column, country, fires_column)
+
 print(fires)
