9f5ac3b9738f6ffa479e96aa16c3e235c867f855
Jackson Fairborn <jdfairborn@gmail.com>
2026-08-30 14:55:42 -0600

    Added ability to input string name of query and result columns

--- my_utils.py
+++ my_utils.py
@@ -1,17 +1,25 @@
 import csv
 
 def get_column(file_name, query_column, query_value, result_column):
+
     file = open(file_name, mode='r', encoding='utf-8', newline='')
-    
-    reader = csv.reader(file)
+
+    if type(query_column) == str:
+        reader = csv.DictReader(file)
+        if query_column not in reader.fieldnames:
+            print(f"Error: Query ('{query_column}') not found. Defaulting to {reader.fieldnames[0]}.")
+        if result_column not in reader.fieldnames:
+            print(f"Error: Result ('{result_column}') not found. Defaulting to {reader.fieldnames[1]}.")
+    else:
+        reader = csv.reader(file)
 
     results = []
 
     for row in reader:
-
         if row[query_column] == query_value:
             results.append(row[result_column])
 
     file.close()
 
     return results
+
--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,8 @@
 from my_utils import get_column
 
 country='United States of America'
-country_column = 0
-fires_column = 3
+country_column = 'Area'
+fires_column = 'Forest fires'
 file_name = 'Agrofood_co2_emission.csv'
 fires = get_column(file_name, country_column, country, fires_column)
 
