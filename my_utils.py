0ae28776a9ffed70e921dfa9cfc757f466a17851
Jackson Fairborn <jdfairborn@gmail.com>
2026-08-30 14:29:38 -0600

    Tentative get_cols fix

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,17 @@
+import csv
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    file = open(file_name, mode='r', encoding='utf-8', newline='')
+    
+    reader = csv.reader(file)
+
+    results = []
+
+    for row in reader:
+
+        if row[query_column] == query_value:
+            results.append(row[result_column])
+
+    file.close()
+
+    return results
