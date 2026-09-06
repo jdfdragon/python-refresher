82d6ac9446c162f0e7656eecaef6b580fbf76e84
Jackson Fairborn <jdfairborn@gmail.com>
2026-09-06 15:10:05 -0600

    Added error handling for get_column

--- my_utils.py
+++ my_utils.py
@@ -1,34 +1,59 @@
+from pathlib import Path
 import csv
 
 
 def get_column(file_name, query_column, query_value, result_column=1):
 
-    file = open(file_name, mode='r', encoding='utf-8', newline='')
+    with open(file_name, mode='r', encoding='utf-8', newline='') as file:
 
-    reader = csv.DictReader(file)
+        file_path = Path(file_name)
 
-    if type(query_column) is str:
-        if query_column not in reader.fieldnames:
-            print(f"Error: Query ('{query_column}') not found."
-                  f" Defaulting to {reader.fieldnames[0]}.")
-            query_column = reader.fieldnames[0]
-    else:
-        query_column = reader.fieldnames[query_column]
+        if not file_path.is_file():
+            print(f"Error: File '{file_name}' not found.")
+            return []
+        
+        reader = csv.DictReader(file)
+        fieldnames = reader.fieldnames
 
-    if type(result_column) is str:
-        if result_column not in reader.fieldnames:
-            print(f"Error: Result ('{result_column}') not found."
-                  f" Defaulting to {reader.fieldnames[1]}.")
-            result_column = reader.fieldnames[1]
-    else:
-        result_column = reader.fieldnames[result_column]
+        if not fieldnames:
+            print("Error: File is empty or does not contain a header row.")
+            
 
-    results = []
+        if isinstance(query_column, str):
+            if query_column not in reader.fieldnames:
+                print(f"Error: Query ('{query_column}') not found."
+                    f" Defaulting to {reader.fieldnames[0]}.")
+                query_column = reader.fieldnames[0]
+        elif isinstance(query_column, int):
+            if 0 <= query_column < len(reader.fieldnames):
+                query_column = reader.fieldnames[query_column]
+            else:
+                print(f"Error: Query column index ({query_column}) is out of bounds."
+                    f" Defaulting to {reader.fieldnames[0]}.")
+                query_column = reader.fieldnames[0]
 
-    for row in reader:
-        if row[query_column] == query_value:
-            results.append(row[result_column])
+        if type(result_column) is str:
+            if result_column not in reader.fieldnames:
+                print(f"Error: Result ('{result_column}') not found."
+                    f" Defaulting to {reader.fieldnames[1]}.")
+                result_column = reader.fieldnames[1]
+        elif isinstance(result_column, int):
+            if 0 <= result_column < len(reader.fieldnames):
+                result_column = reader.fieldnames[result_column]
+            else:
+                print(f"Error: Result column index ({result_column}) is out of bounds."
+                    f" Defaulting to {reader.fieldnames[1]}.")
+                result_column = reader.fieldnames[1]
 
-    file.close()
+        results = []
+
+        for row in reader:
+            if row[query_column] == query_value:
+                val = row[result_column]
+                val = val.replace(',', '')
+                val.replace(' ', '')
+                decimal = float(val)
+                integer = int(decimal)
+                results.append(integer)
 
     return results
