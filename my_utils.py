4cbad693ed1dc5c1f752aed0e86737a2aba36ec4
Jackson Fairborn <jdfairborn@gmail.com>
2026-09-06 15:13:28 -0600

    Updated my_utils to match style

--- my_utils.py
+++ my_utils.py
@@ -11,38 +11,37 @@ def get_column(file_name, query_column, query_value, result_column=1):
         if not file_path.is_file():
             print(f"Error: File '{file_name}' not found.")
             return []
-        
+
         reader = csv.DictReader(file)
         fieldnames = reader.fieldnames
 
         if not fieldnames:
             print("Error: File is empty or does not contain a header row.")
-            
 
         if isinstance(query_column, str):
             if query_column not in reader.fieldnames:
                 print(f"Error: Query ('{query_column}') not found."
-                    f" Defaulting to {reader.fieldnames[0]}.")
+                      f" Defaulting to {reader.fieldnames[0]}.")
                 query_column = reader.fieldnames[0]
         elif isinstance(query_column, int):
             if 0 <= query_column < len(reader.fieldnames):
                 query_column = reader.fieldnames[query_column]
             else:
-                print(f"Error: Query column index ({query_column}) is out of bounds."
-                    f" Defaulting to {reader.fieldnames[0]}.")
+                print(f"Error: Query index ({query_column}) out of bounds."
+                      f" Defaulting to {reader.fieldnames[0]}.")
                 query_column = reader.fieldnames[0]
 
         if type(result_column) is str:
             if result_column not in reader.fieldnames:
                 print(f"Error: Result ('{result_column}') not found."
-                    f" Defaulting to {reader.fieldnames[1]}.")
+                      f" Defaulting to {reader.fieldnames[1]}.")
                 result_column = reader.fieldnames[1]
         elif isinstance(result_column, int):
             if 0 <= result_column < len(reader.fieldnames):
                 result_column = reader.fieldnames[result_column]
             else:
-                print(f"Error: Result column index ({result_column}) is out of bounds."
-                    f" Defaulting to {reader.fieldnames[1]}.")
+                print(f"Error: Result index ({result_column}) out of bounds."
+                      f" Defaulting to {reader.fieldnames[1]}.")
                 result_column = reader.fieldnames[1]
 
         results = []
