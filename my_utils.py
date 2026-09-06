f398328a39fd9df2d61672ff718d475f455f6917
Jackson Fairborn <jdfairborn@gmail.com>
2026-09-06 15:53:52 -0600

    Fixed argparser implementation

--- my_utils.py
+++ my_utils.py
@@ -1,3 +1,4 @@
+from ast import In
 from pathlib import Path
 import csv
 
@@ -18,31 +19,31 @@ def get_column(file_name, query_column, query_value, result_column=1):
         if not fieldnames:
             print("Error: File is empty or does not contain a header row.")
 
-        if isinstance(query_column, str):
-            if query_column not in reader.fieldnames:
+        try:
+            query_column = int(query_column)
+            query_column = fieldnames[query_column]
+        except (ValueError, TypeError):
+            if query_column not in fieldnames:
                 print(f"Error: Query ('{query_column}') not found."
-                      f" Defaulting to {reader.fieldnames[0]}.")
-                query_column = reader.fieldnames[0]
-        elif isinstance(query_column, int):
-            if 0 <= query_column < len(reader.fieldnames):
-                query_column = reader.fieldnames[query_column]
-            else:
-                print(f"Error: Query index ({query_column}) out of bounds."
-                      f" Defaulting to {reader.fieldnames[0]}.")
-                query_column = reader.fieldnames[0]
+                      f" Defaulting to {fieldnames[0]}.")
+                query_column = fieldnames[0]
+        except IndexError:
+            print(f"Error: Query index ({query_column}) out of bounds."
+                  f" Defaulting to {fieldnames[0]}.")
+            query_column = fieldnames[0]
 
-        if type(result_column) is str:
-            if result_column not in reader.fieldnames:
+        try:
+            result_column = int(result_column)
+            result_column = fieldnames[result_column]
+        except (ValueError, TypeError):
+            if result_column not in fieldnames:
                 print(f"Error: Result ('{result_column}') not found."
-                      f" Defaulting to {reader.fieldnames[1]}.")
-                result_column = reader.fieldnames[1]
-        elif isinstance(result_column, int):
-            if 0 <= result_column < len(reader.fieldnames):
-                result_column = reader.fieldnames[result_column]
-            else:
-                print(f"Error: Result index ({result_column}) out of bounds."
-                      f" Defaulting to {reader.fieldnames[1]}.")
-                result_column = reader.fieldnames[1]
+                      f" Defaulting to {fieldnames[1]}.")
+                result_column = fieldnames[1]
+        except IndexError:
+            print(f"Error: Result index ({result_column}) out of bounds."
+                  f" Defaulting to {fieldnames[1]}.")
+            result_column = fieldnames[1]
 
         results = []
 
@@ -56,3 +57,4 @@ def get_column(file_name, query_column, query_value, result_column=1):
                 results.append(integer)
 
     return results
+
--- print_fires.py
+++ print_fires.py
@@ -5,14 +5,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument("file_name")
 parser.add_argument("query_column")
 parser.add_argument("query_value")
-parser.add_argument("result_column", default=1)
+parser.add_argument("result_column", nargs='?', default=1)
 
 args = parser.parse_args()
 
-country = args.query_value
-country_column = args.query_column
-fires_column = args.result_column
+query_value = args.query_value
+query_column = args.query_column
+result_column = args.result_column
 file_name = args.file_name
-fires = get_column(file_name, country_column, country, fires_column)
+fires = get_column(file_name, query_column, query_value, result_column)
 
 print(fires)
